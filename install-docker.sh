68f88deed987814ac897c16870afa8bbfa5fb72a
UZabauski <uzabauski@gmail.com>
2021-02-13 23:41:25 +0300

    Added docker-compose

--- install-docker.sh
+++ install-docker.sh
@@ -19,3 +19,10 @@ sudo apt install docker-ce -y
 
 # Add a user to the docker group
 sudo usermod -aG docker vagrant
+
+# Optional: install docker-compose
+# Check latest compose version: https://github.com/docker/compose/releases
+DOCKER_COMPOSE=1.28.2
+
+sudo curl -L "https://github.com/docker/compose/releases/download/$DOCKER_COMPOSE/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
